244a6ece8e37db31d85b3606dca477009ebf80e8
Alexander McFarland <alex.925@gmail.com>
2024-12-10 13:28:10 -0500

    tests

---
+++ tests/end_to_end/test_end_to_end_failure.py
@@ -0,0 +1,14 @@
+import shutil
+import os
+from os.path import join as pjoin
+
+
+def test_fail(setup_temp_dir):
+	"""
+	Test behavior of docker when AAVengeR fails inside of the docker container.
+	"""
+
+	print(setup_temp_dir)	
+
+	
+
---
+++ tests/unit/test_aavenger_test.py
@@ -0,0 +1,20 @@
+from unittest.mock import patch, Mock, call
+import pytest
+from os.path import join as pjoin
+from lib.common import RunHandler, AAVengeRDockerRunner
+
+
+def test_initalize_aavenger_test(example_run_handler_read_write):
+	"""
+	pytest -sv tests/unit/test_aavenger_test.py::test_initalize_aavenger_test
+	"""
+
+	example_run_handler_read_write.config_path = pjoin(example_run_handler_read_write.input_manager.raw_dir, 'config.yml')
+
+	aavenger_run = AAVengeRDockerRunner(run_handler = example_run_handler_read_write)
+
+	print(aavenger_run.__dict__)
+
+	pass
+
+
