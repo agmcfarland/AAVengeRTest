573bb57c81cc47c9c0b2b3c9dcadd88f8d36612e
Alexander McFarland <alex.925@gmail.com>
2024-12-18 14:03:14 -0500

    logging finished result

--- src/run.sh
+++ src/run.sh
@@ -45,7 +45,7 @@ head "${DST_PATH}/${AAVENGER_TO_TEST}/version/version"
 # Packaged test data (small) (15 mins)
 AAVengeRTest \
 --output_dir /data/aavenger_stability_testing/packaged_test_data \
---test_data_sheet /data/AAVengeRTest/tests/data/ete_correct_input_sheet.csv \
+--test_data_sheet /data/aavenger_stability_testing/data/packaged_test_data/ete_correct_input_sheet.csv \
 --expected_output_dir /data/aavenger_stability_testing/data/packaged_test_data/expected_results \
 --microb120_user agmcfarland \
 --docker_image_name aavenger_docker_v3 \
